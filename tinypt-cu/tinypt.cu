d1fbf3a766e4e19fc2ec3e775e02bf567670ced9
gusrlLee <gusrl3204@naver.com>
2026-01-16 15:35:25 +0900

    add submodule tiny-obj-loader

---
+++ tinypt-cu/tinypt.cu
@@ -0,0 +1,18 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tinyobjloader/tiny_obj_loader.h"
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
